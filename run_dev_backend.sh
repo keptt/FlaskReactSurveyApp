1936c97aac3e3d690d1e0f21d7eff2edb2e3d790
keptt <blackoutxray@gmail.com>
2020-07-03 23:11:57 +0300

    fixed path for virtual env in riun_dev_backend.sh

--- run_dev_backend.sh
+++ run_dev_backend.sh
@@ -1,9 +1,7 @@
-python3 -m venv env
+python3 -m venv flask-backend/env
 . flask-backend/env/Scripts/activate
 
 pip3 install -r flask-backend/requirements.txt
 
 python3 flask-backend/db_init.py
 python3 flask-backend/app.py
-
-
